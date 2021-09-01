e5cc099247596e55e6dd9f263ec8789903d1d506
C. Eduardo Carrasco <educamo@gmail.com>
2021-09-01 15:54:25 -0500

    se elimino el ajax

--- source/js/index.js
+++ source/js/index.js
@@ -18,43 +18,4 @@ $(document).ready(function() {
         $(this).val($(this).val().replace(/[^a-zA-Z ]/, ''));
     });
 
-    //evento submit del formulario para enviar los datos por mail mediante ajax
-    $("#frmregistro").submit(function(event) {
-        event.preventDefault();
-
-        var formData = $(this).serialize();
-        console.log(formData);
-
-        $.ajax({
-            type: $(this).attr("method"),
-            url: "php/mail.php",
-            data: formData,
-            cache: true,
-            processData: true,
-
-            beforeSend: function() {
-                $("#btnsubmit").html("Enviando...");
-                $("#btnsubmit").attr("disabled", "disabled");
-            },
-            success: function(data) {
-                $("#massage").removeClass("massageerror");
-                $("#massage").removeClass("massage");
-                $("#massage").addClass("massage");
-                $("#massage").show();
-                $("#massage").html(JSON.parse(data));
-                console.log(data);
-                location.reload(true);
-            },
-            error: function(data) {
-                $("#massage").removeClass("massage");
-                $("#massage").addClass("massageerror");
-                $("#massage").show();
-                $("#massage").html(JSON.parse(data));
-                $("#btnsubmit").html("Enviar");
-                $("#btnsubmit").attr("disabled", "enable");
-            },
-        });
-        return true;
-    });
-
 });
